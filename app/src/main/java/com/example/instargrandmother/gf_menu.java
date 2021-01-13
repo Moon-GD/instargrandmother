2875b0974f9d04220e283800a742d79b851cdd0f
PC-GD\문경덕 <king4mun@gmail.com>
2021-01-13 16:24:49 +0900

    할아버지 방-뉴스, 날씨 완성

--- app/src/main/java/com/example/instargrandmother/gf_menu.java
+++ app/src/main/java/com/example/instargrandmother/gf_menu.java
@@ -1,6 +1,7 @@
 package com.example.instargrandmother;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,12 +9,16 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class gf_menu extends AppCompatActivity {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gf_menu);
 
         Button previous = (Button) findViewById(R.id.previous_gf);
+        Button weather = (Button) findViewById(R.id.button_weather_gf);
+        Button news = (Button) findViewById(R.id.button_news_gf);
 
         previous.setOnClickListener(new View.OnClickListener(){
 
@@ -21,6 +26,30 @@ public class gf_menu extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                Intent intent_weather = getPackageManager().getLaunchIntentForPackage("com.lifeoverflow.app.weather");
+                startActivity(intent_weather);
+                }
+                catch(Exception e){
+                    Intent marketLaunch = new Intent(Intent.ACTION_VIEW);
+                    marketLaunch.setData(Uri.parse("https://play.google.com/store/apps/details?id=com.lifeoverflow.app.weather"));
+                    startActivity(marketLaunch);
+                }
+            }
+        });
+
+        news.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent naver_news_site = new Intent(Intent.ACTION_VIEW);
+                naver_news_site.setData(Uri.parse("https://news.naver.com/"));
+                startActivity(naver_news_site);
             }
         });
     }
